5a5c890a271fb6a024877915e6330c67f002dddb
term7 <term7@term7.info>
2024-07-29 15:49:14 +0200

    comment

--- Install_DaemonLeash.sh
+++ Install_DaemonLeash.sh
@@ -292,10 +292,18 @@ EOF
 
 sudo tee "$UNLOAD_HELPER" << EOF > /dev/null
 #!/bin/bash
+
+#   Run LocalUnloadList.sh list once directly after startup (to make sure no Adobe Software has been started)
 $USER_UNLOAD_LIST
+
+#   Variable that checks for Adobe Software that has been started by the user:
 ApplicationCheck() {
 ps cax | grep -v grep | grep -v LogTransport2 | grep -E "Acrobat|InDesign|Illustrator|Dreamweaver|Photoshop|Lightroom|Audition|Premiere Pro|Media Encoder|After Effects|Creative Cloud" > /dev/null
 }
+
+#   If Adobe Software that has been started by the user is running: wait for 60 seconds, then check again
+#   If no Adobe Software that has been started by the user is running: terminate all Adobe Processes and repeat
+
 while true
 do
 if ApplicationCheck
